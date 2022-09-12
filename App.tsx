39342d320e7729f347dd152ad98e5572074b8cdb
alexrelance <alexrelance@gmail.com>
2022-09-12 12:47:55 -0300

    add react native

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Send 1" />
+      <Button title="Send 1" />
+      <Button title="Send 1" />
+      <Button title="Hello World" />
+      
       <StatusBar style="auto" />
     </View>
   );
